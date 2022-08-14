8cad7f4551a92ef799e90e414435ab2cd8a06600
Dan Colmenares <dacolmenares93@gmail.com>
2022-08-14 10:08:12 -0500

    Feat: Starting again my blog

--- utils/LinkServices.ts
+++ utils/LinkServices.ts
@@ -4,9 +4,10 @@ const linkServices = {
   fetchLinksDatabase: async () => {
     const { results } = await notion.databases.query({
       database_id: LINK_TREE_ID,
-      page_size: 20,
       sorts: [{ property: "Orden", direction: "ascending" }],
     });
+
+    return results;
 
     return results
       .map((link: any) => link.properties)
