03b6105467d266b89daf4b4b8fb86190b28f12fd
Dan Colmenares <dacolmenares93@gmail.com>
2022-08-20 18:29:28 -0500

    Feat: Add DayJS

--- pages/Blog/[slug].vue
+++ pages/Blog/[slug].vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 const route = useRoute();
+import dayjs from "dayjs";
 
 const { slug } = route.params;
-const { data, pending } = await useLazyAsyncData("/api/blog/get-post/", () =>
+const { data, pending, refresh } = await useLazyAsyncData("content", () =>
   $fetch(`/api/blog/get-post/${slug}`)
 );
 
 // TODO: Research Meta SEO for blogs
+refresh();
 // definePageMeta({
-//   title: computed(() => `Daniel Colmenares - ${data.value.properties.Post}`),
+//   title: `Daniel Colmenares - ${data?.value.properties.Post || ""}`,
 // });
 </script>
 
@@ -17,16 +19,19 @@ const { data, pending } = await useLazyAsyncData("/api/blog/get-post/", () =>
     <LoadingPage loadMessage="Cargando Publicación" v-if="pending" />
 
     <section v-else class="flex flex-col justify-center space-y-8">
-      <header class="flex flex-col space-y-4">
+      <header class="flex flex-col w-full space-y-4">
         <picture class="w-full md:h-[400px] overflow-hidden">
           <img
             :src="data.properties.Image_URL"
             :alt="`Banner ${data.properties.Post}`"
-            class="object-center objet-fill"
+            class="object-fill object-center w-full"
           />
         </picture>
         <Heading1>{{ data.properties.Post }}</Heading1>
-        <p>Publicado el {{ data.properties.Fecha_Publicacion }}</p>
+        <p>
+          Publicado el
+          {{ dayjs(data.properties.Fecha_Publicacion).format("DD MMMM YYYY") }}
+        </p>
       </header>
 
       <Divider />
