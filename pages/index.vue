fafa83e2f23c1719240b3b211a952ca9f515dcd5
Dan Colmenares <dacolmenares93@gmail.com>
2022-10-09 19:27:49 -0500

    Fix: Solve issues with Nuxt3 rc-11 and vue use motion

--- pages/index.vue
+++ pages/index.vue
@@ -20,3 +20,14 @@ definePageMeta({
     </section>
   </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
