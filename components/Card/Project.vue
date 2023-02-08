4cabc251402c27a880e86dc35b39558ab8b953d0
Daniel <dacolmenares93@gmail.com>
2023-02-07 21:22:46 -0500

    Feat: Improving project card hover

--- components/Card/Project.vue
+++ components/Card/Project.vue
@@ -5,32 +5,49 @@ interface Props {
   projectProps: IProjectProperties;
 }
 
+const target = ref();
+const { isOutside } = useMouseInElement(target);
+
 const { projectProps } = defineProps<Props>();
 const { Name, Slug, Tags, Preview } = projectProps;
 </script>
 
 <template>
   <article
-    class="flex flex-col justify-between space-y-2 overflow-hidden duration-300 transform rounded-lg shadow-lg cursor-pointer h-80 bg-primary hover:scale-110"
+    class="relative flex flex-col justify-between space-y-2 overflow-hidden duration-300 transform rounded-lg shadow-lg cursor-pointer hover:scale-105"
+    ref="target"
     @click="navigateTo(`/portfolio/projects/${Slug}`)"
   >
-    <header class="relative overflow-hidden h-3/5">
-      <img
-        :src="Preview"
-        :alt="`${Name} preview`"
-        class="absolute top-0 left-0 right-0 w-full"
-        loading="lazy"
-      />
-    </header>
+    <img
+      :src="Preview"
+      :alt="`${Name} preview`"
+      class="object-cover w-full aspect-square"
+      loading="lazy"
+    />
 
-    <section class="flex flex-col p-4 space-y-2 h-1/5">
-      <Heading2 customClass="text-gold">{{ Name }}</Heading2>
-    </section>
+    <transition-slide :offset="[0, '100%']">
+      <footer
+        v-if="!isOutside"
+        class="absolute bottom-0 left-0 right-0 z-10 flex-col justify-start hidden gap-2 px-4 py-2 lg:flex h-1/3 bg-primary"
+      >
+        <Heading2 customClass="text-gold">{{ Name }}</Heading2>
+        <section class="flex flex-wrap items-center justify-start gap-1">
+          <Pill v-for="(name, idx) in Tags" :key="idx">
+            {{ name }}
+          </Pill>
+        </section>
+      </footer>
+    </transition-slide>
 
-    <footer class="flex flex-wrap items-center justify-start gap-1 p-4 h-1/5">
-      <Pill v-for="(name, idx) in Tags" :key="idx">
-        {{ name }}
-      </Pill>
-    </footer>
+    <footer
+        class="absolute bottom-0 left-0 right-0 z-10 flex flex-col justify-start gap-1 px-4 py-2 lg:hidden h-2/5 bg-primary"
+      >
+        <Heading2 customClass="text-gold">{{ Name }}</Heading2>
+        <section class="flex flex-wrap items-center justify-start gap-1">
+          <Pill v-for="(name, idx) in Tags" :key="idx">
+            {{ name }}
+          </Pill>
+        </section>
+      </footer>
   </article>
 </template>
